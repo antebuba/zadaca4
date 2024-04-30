b56a1dd62e2016d274ec27ca7d644d5e4f4b828e
antebuba <ante.bubalo@fsre.sum.ba>
2024-04-30 10:58:23 +0200

    fourth commit

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,67 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\CustomersController;
+use App\Http\Controllers\OrdersController;
+use App\Http\Controllers\ProductsController;
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+// Prikazuje sve proizvode
+Route::get('/products', [ProductsController::class, 'index']);
+
+// Dodaje novi proizvod
+Route::post('/products', [ProductsController::class, 'store']);
+
+// Prikazuje određeni proizvod
+Route::get('/products/{ProductID}', [ProductsController::class, 'show']);
+
+// Ažurira određeni proizvod
+Route::put('/product/{ProductID}', [ProductsController::class, 'update']);
+
+// Briše određeni proizvod
+Route::delete('/products/{ProductID}', [ProductsController::class, 'destroy']);
+
+
+
+
+// Prikazuje sve proizvode
+Route::get('/customers', [CustomersController::class, 'index']);
+
+// Dodaje novi proizvod
+Route::post('/customers', [CustomersController::class, 'store']);
+
+// Prikazuje određeni proizvod
+Route::get('/customers/{CustomerID}', [CustomersController::class, 'show']);
+
+// Ažurira određeni proizvod
+Route::put('/customer/{CustomerID}', [CustomersController::class, 'update']);
+
+// Briše određeni proizvod
+Route::delete('/customers/{CustomerID}', [CustomersController::class, 'destroy']);
+
+
+
+
+
+// Prikazuje sve proizvode
+Route::get('/orders', [OrdersController::class, 'index']);
+
+// Dodaje novi proizvod
+Route::post('/orders', [OrdersController::class, 'store']);
+
+// Prikazuje određeni proizvod
+Route::get('/orders/{OrderID}', [OrdersController::class, 'show']);
+
+// Ažurira određeni proizvod
+Route::put('/order/{OrderID}', [OrdersController::class, 'update']);
+
+// Briše određeni proizvod
+Route::delete('/orders/{OrderID}', [OrdersController::class, 'destroy']);
